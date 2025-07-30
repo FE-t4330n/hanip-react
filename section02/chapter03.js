54672017fdcfc83b246e0c6c1adf7f562f012446
FE-t4330n <t4330n@gmail.com>
2025-07-30 17:39:52 +0900

    2.3) 구조분해할당

---
+++ section02/chapter03.js
@@ -0,0 +1,20 @@
+// 1. 배열 구조 분해 할당
+let arr = [123];
+
+let [one, two, three] = arr;
+
+// 2. 객체 구조 분해 할당
+let person = {
+    name: 't4330n',
+    age: 27,
+    hobby: 'soccer',
+};
+
+let { name, age, hobby, extra = 'hello' } = person;
+
+// 3. 객체 구조 분해 할당을 이용해서 함수의 매개변수를 받는 방법
+const func = ({ name, age, hobby, extra }) => {
+    console.log(name, age, hobby, extra);
+};
+
+func(person);
