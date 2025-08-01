1ce5be8d3d67394f0639cd651d61a809c4b9af60
FE-t4330n <t4330n@gmail.com>
2025-08-01 15:22:08 +0900

    3.5) Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,5 +1,10 @@
 import multiply, { add, sub } from './math.js';
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multiply(3, 4));
+import randomColor from 'randomcolor';
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multiply(3, 4));
