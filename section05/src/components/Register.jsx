aaf1a44fc833c29ae47b926a5b63d013c605f0cc
FE-t4330n <t4330n@gmail.com>
2025-08-05 16:58:15 +0900

    5.10) useRef로 컴포넌트의 변수 생성하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 // 간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
 // 3. 국적
 // 4. 자기소개
+
+// let count = 0;
 
 const Register = () => {
     const [input, setInput] = useState({
@@ -14,7 +16,16 @@ const Register = () => {
         bio: '',
     });
 
+    const countRef = useRef(0);
+    const inputRef = useRef();
+
+    // let count = 0;
+
     const onChange = (e) => {
+        countRef.current++;
+        console.log(countRef.current);
+        // count++;
+        // console.log(count);
         console.log(e.target.name, e.target.value);
         setInput({
             ...input,
@@ -22,10 +33,19 @@ const Register = () => {
         });
     };
 
+    const onSubmit = (e) => {
+        if (input.name === '') {
+            // 이름을 입력하는 DOM 요소 포커스
+            inputRef.current.focues();
+        }
+    };
+
     return (
         <>
+            <div></div>
             <div>
                 <input
+                    ref={inputRef}
                     name="name"
                     value={input.name}
                     onChange={onChange}
